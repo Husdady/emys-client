5bdecd957b9b21a722ffbf28d95058e3d76dc43c
Husdady Mena <husdadymena@gmail.com>
2023-11-12 23:20:50 -0500

    Fix ts errors

--- src/utils/isFunction.ts
+++ src/utils/isFunction.ts
@@ -1,8 +1,9 @@
+/* eslint-disable no-unused-vars */
 /**
  * Check if the func received by parameters is an function
  * @param {unknown} func function as parameter
  * @returns {boolean} Boolean
  */
-export default function isFunction(func: unknown): func is () => void {
+export default function isFunction<T>(func: unknown): func is (data: T) => void {
   return typeof func === 'function'
 }
