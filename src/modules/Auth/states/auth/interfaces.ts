9c3408045be894b02e89b4807bb4da452bd82e64
Husdady Mena <husdadymena@gmail.com>
2023-11-15 22:52:45 -0500

    Fix TS errors

--- src/modules/Auth/states/auth/interfaces.ts
+++ src/modules/Auth/states/auth/interfaces.ts
@@ -1,10 +1,10 @@
 // Interfaces
-import { Role } from '@modules/Roles/api/interfaces'
+// import { Role } from '@modules/Roles/api/interfaces'
 import { Image, TimeStamps } from '@libs/axios/interfaces'
-import { Region } from '@modules/Ubigeo/modules/Regions/api/interfaces'
-import { Country } from '@modules/Ubigeo/modules/Countries/api/interfaces'
-import { District } from '@modules/Ubigeo/modules/Districts/api/interfaces'
-import { Province } from '@modules/Ubigeo/modules/Provinces/api/interfaces'
+// import { Region } from '@modules/Ubigeo/modules/Regions/api/interfaces'
+// import { Country } from '@modules/Ubigeo/modules/Countries/api/interfaces'
+// import { District } from '@modules/Ubigeo/modules/Districts/api/interfaces'
+// import { Province } from '@modules/Ubigeo/modules/Provinces/api/interfaces'
 
 export interface User extends TimeStamps {
   id: string
@@ -12,12 +12,12 @@ export interface User extends TimeStamps {
   email: string
   status: string
   secretKey: string
-  country?: Country
-  region?: Region
-  province?: Province
-  district?: District
+  // country?: Country
+  // region?: Region
+  // province?: Province
+  // district?: District
   profilePhoto?: Image | null
-  role: Omit<Role, 'viewsId' | 'permissionsId'>
+  // role: Omit<Role, 'viewsId' | 'permissionsId'>
 }
 
 export interface AuthState {
