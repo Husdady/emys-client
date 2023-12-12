ff3122325bfcd80d6e74d1af0321c91181062d9c
Husdady Mena <husdadymena@gmail.com>
2023-12-12 18:47:16 -0500

    Fix TS errors

--- src/modules/Auth/loaders/validate-token.ts
+++ src/modules/Auth/loaders/validate-token.ts
@@ -12,7 +12,7 @@ import isString from '@utils/isString'
 import isEmptyString from '@utils/isEmptyString'
 
 // Constants
-import { DEFAULT_LOADER_RESPONSE } from '@assets/loaders'
+import { DEFAULT_LOADER_RESPONSE } from '@assets/data/loaders'
 
 /**
  * Validate API token
