d0475046ba5f27d92284042a62c6a6df2f985120
Husdady Mena <husdadymena@gmail.com>
2024-01-05 12:46:11 -0500

    Improve Home Card design

--- src/modules/Home/components/OmnilifeCards/index.tsx
+++ src/modules/Home/components/OmnilifeCards/index.tsx
@@ -3,7 +3,7 @@ import Cards from './Cards'
 
 export default function OmnilifeCards() {
   return (
-    <section className="omnilife-cards bg-purple-100 dark:bg-purple-500 py-[3rem]">
+    <section className="omnilife-cards bg-purple-100 dark:bg-purple-500 py-[3rem] px-[1.85rem]">
       <div className="inner-wrapper max-w-[1105px] mx-auto w-full h-full">
         <Cards />
       </div>
