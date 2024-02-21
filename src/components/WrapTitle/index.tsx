c7ed7c403519df2c61b8252bc34d49ba11c3fb81
Husdady Mena <husdadymena@gmail.com>
2024-02-21 12:57:01 -0500

    Improve colors

--- src/components/WrapTitle/index.tsx
+++ src/components/WrapTitle/index.tsx
@@ -13,7 +13,7 @@ const Help = lazy(() => import('./Help'))
 export default function BoxWrapTitle({ icon, value, popupTitle }: BoxWrapTitleProps) {
   return (
     <section className="box-title min-h-[46.79px] flex items-center justify-between rounded-md pl-4 overflow-hidden pr-2 py-[0.40rem] bg-white shadow-md gap-x-12 text-gray-700 dark:shadow-none dark:bg-gray-900 dark:text-gray-400">
-      <div className="flex items-center gap-x-2 dark:text-pink-200">
+      <div className="flex items-center gap-x-2 dark:text-pink-300">
         {icon}
         <h5 className="font-bold dark:font-semibold text-base font-poppins text-inherit leading-snug">{value}</h5>
       </div>
