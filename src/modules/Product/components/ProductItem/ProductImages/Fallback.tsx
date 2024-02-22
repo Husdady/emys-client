88f1b1fdedda7ec7d7fcada05dd2cf948340aa37
Husdady Mena <husdadymena@gmail.com>
2024-02-21 22:59:50 -0500

    Release 0.0.12

--- src/modules/Product/components/ProductItem/ProductImages/Fallback.tsx
+++ src/modules/Product/components/ProductItem/ProductImages/Fallback.tsx
@@ -1,3 +1,5 @@
 export default function Fallback() {
-  return <aside className="product-images"></aside>
+  return (
+    <aside className="product-images bg-white pt-3.5 pb-4 px-4 font-poppins rounded shadow-lg min-h-[500px] dark:shadow-none dark:bg-gray-900"></aside>
+  )
 }
