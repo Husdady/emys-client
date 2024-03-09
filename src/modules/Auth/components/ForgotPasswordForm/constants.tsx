7a2ac1c6c3bfeb2497723e89ed26f4514fcbd5da
Husdady Mena <husdadymena@gmail.com>
2024-03-09 14:22:05 -0500

    Fix TS errors

--- src/modules/Auth/components/ForgotPasswordForm/constants.tsx
+++ src/modules/Auth/components/ForgotPasswordForm/constants.tsx
@@ -5,7 +5,7 @@ import BrandGmail from '@components/Icons/BrandGmail'
 import { InputLabelProps } from '@components/InputLabel/interfaces'
 
 // Constants
-import { defaultIconProps } from '@components/InputLabel'
+import { defaultIconProps } from '@components/InputLabel/constants'
 
 export const NOTE_MESSAGE =
   'Si no has recibido la verificación en tu correo electrónico, prueba solicitando una nueva verificación en el botón de abajo. Se enviará un mensaje a tu correo electrónico, no olvides buscar en la carpeta de SPAM'
