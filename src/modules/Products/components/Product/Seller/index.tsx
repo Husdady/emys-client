2ca1cf7acaae633216da1416734088ac9d7b0bff
Husdady Mena <husdadymena@gmail.com>
2024-03-13 18:20:07 -0500

    Fix TS errors

--- src/modules/Products/components/Product/Seller/index.tsx
+++ src/modules/Products/components/Product/Seller/index.tsx
@@ -3,13 +3,13 @@ import dynamic from 'next/dynamic'
 
 // Interfaces
 import { Image } from '@libs/axios/interfaces'
-import { Seller } from '@modules/Sellers/api/interfaces'
+import { Seller as SellerModel } from '@modules/Sellers/api/interfaces'
 
 // Dynamic Components
 const SellerPhoto = dynamic(() => import('./Photo'))
 const SellerFullname = dynamic(() => import('./Fullname'))
 
-export default function Seller({ photo, fullname }: Partial<Seller> = {}) {
+export default function Seller({ photo, fullname }: Partial<SellerModel> = {}) {
   return (
     <div className="seller flex flex-wrap gap-y-1 items-start mt-2 gap-x-1.5 px-3 py-2 bg-gray-100 rounded-lg dark:bg-gray-600">
       <SellerPhoto photo={photo as Image} />
