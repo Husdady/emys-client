144a4f9934fcc42d57df7e2b0dba71b1fc634bf7
Husdady Mena <husdadymena@gmail.com>
2024-03-13 17:28:57 -0500

    Fix nprogress

--- src/hooks/useNprogressDone.ts
+++ src/hooks/useNprogressDone.ts
@@ -1,18 +1,25 @@
 // Librarys
+import Router from 'next/router'
 import NProgress from 'nprogress'
 
 // Hooks
 import useMounted from './useMounted'
-import { usePathname, useSearchParams } from 'next/navigation'
 
 /**
  * Hook for hide the Nprogress bar
  */
 export default function useNprogressDone() {
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
   useMounted(() => {
-    NProgress.done()
-  }, [pathname, searchParams])
+    Router.events.off('routeChangeStart', () => {
+      NProgress.start() // Asegúrate de desvincular el evento al desmontar el componente
+    })
+
+    Router.events.off('routeChangeComplete', () => {
+      NProgress.done() // Asegúrate de desvincular el evento al desmontar el componente
+    })
+
+    Router.events.off('routeChangeError', () => {
+      NProgress.done() // Asegúrate de desvincular el evento al desmontar el componente
+    })
+  }, [])
 }
