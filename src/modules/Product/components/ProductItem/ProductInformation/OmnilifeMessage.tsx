8b347ad39f623dcb1da69cbd461c9e7ca5591bb8
Husdady Mena <husdadymena@gmail.com>
2024-03-20 19:06:04 -0500

    Release 0.0.33

--- src/modules/Product/components/ProductItem/ProductInformation/OmnilifeMessage.tsx
+++ src/modules/Product/components/ProductItem/ProductInformation/OmnilifeMessage.tsx
@@ -3,7 +3,7 @@ import AlertMessage from '@components/AlertMessage'
 
 export default function OmnilifeMessage() {
   return (
-    <div className="mt-4">
+    <div className="mb-5 sm:mb-0 mt-1.5 sm:mt-5 xl:mt-4">
       <AlertMessage>
         NOTA IMPORTANTE: Los productos Omnilife y Seytú tiene una regla de ética, en la cuál no se
         permite vender directamente por línea estos productos. Cada uno de estos productos serán
