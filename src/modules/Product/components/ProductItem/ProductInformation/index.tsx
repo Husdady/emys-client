c3448d1f309e007389f1612756f65da36493f7a5
Husdady Mena <husdadymena@gmail.com>
2024-03-22 13:34:10 -0500

    Release 0.0.42

--- src/modules/Product/components/ProductItem/ProductInformation/index.tsx
+++ src/modules/Product/components/ProductItem/ProductInformation/index.tsx
@@ -33,34 +33,36 @@ export default function ProductInformation(product: Product) {
   return (
     <aside
       className={classnames([
-        (product.images?.length ?? 0) > 4 ? 'xl:min-h-[44.38rem]' : 'xl:min-h-[44.1rem]',
+        (product.images?.length ?? 0) > 4 ? 'xl:min-h-[45.19rem]' : 'xl:min-h-[44.1rem]',
         'min-h-[44.15rem] product-information bg-white overflow-hidden pt-3.5 pb-4 font-poppins rounded shadow-lg dark:shadow-none dark:bg-gray-900 flex flex-col justify-between'
       ])}
     >
-      <div className="px-4 xl:pb-4 xl:border-b xl:border-gray-300 dark:xl:border-gray-600">
-        <Header {...product} />
+      <section>
+        <div className="px-4 xl:pb-4 xl:border-b xl:border-gray-300 dark:xl:border-gray-600">
+          <Header {...product} />
 
-        <h3 className="product-name text-[1.75rem] leading-tight break-word font-semibold text-gray-700 dark:text-white">
-          {product.name}
-          {isString(content) && !isEmptyString(content) ? `, ${content}` : ''}
-        </h3>
+          <h3 className="product-name text-[1.75rem] leading-tight break-word font-semibold text-gray-700 dark:text-white">
+            {product.name}
+            {isString(content) && !isEmptyString(content) ? `, ${content}` : ''}
+          </h3>
 
-        <div className="price-container flex items-center justify-between gap-x-6 mt-3 mb-4">
-          <StockTag isInStock={product.isInStock} />
-          <Price price={product.price} currencyType={product.currencyType} />
+          <div className="price-container flex items-center justify-between gap-x-6 mt-3 mb-4">
+            <StockTag isInStock={product.isInStock} />
+            <Price price={product.price} currencyType={product.currencyType} />
+          </div>
+
+          <Stars {...product} />
         </div>
 
-        <Stars {...product} />
-      </div>
-
-      <div
-        ref={innerInformationRef}
-        className="pr-3 inner-information xl:min-h-[19.75rem] xl:max-h-[19.75rem] xl:overflow-y-auto pl-2 ml-2 mr-1 pb-4"
-      >
-        {product.type !== PRODUCT_TYPE && <OmnilifeMessage />}
-        <Tags {...product} />
-        <ProductFields {...product} innerInformationRef={innerInformationRef} />
-      </div>
+        <div
+          ref={innerInformationRef}
+          className="pr-3 inner-information xl:min-h-[19.75rem] xl:max-h-[19.75rem] xl:overflow-y-auto pl-2 ml-2 mr-1 pb-4"
+        >
+          {product.type !== PRODUCT_TYPE && <OmnilifeMessage />}
+          <Tags {...product} />
+          <ProductFields {...product} innerInformationRef={innerInformationRef} />
+        </div>
+      </section>
 
       <div className="pt-2 xl:border-t xl:border-gray-300 dark:xl:border-gray-600 px-4 footer-content">
         <div className="mt-2 units-wrapper flex items-center gap-x-2">
